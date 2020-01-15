084d95acbfcacc8adcdc3f543ce0298ccfd22395
David Balboa <david@devils.local>
2020-01-15 20:53:53 +0100

    firebase activada

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
+  // Your web app's Firebase configuration
+  var mySubString2 = "no"
+
+  var firebaseConfig = {
     apiKey: "AIzaSyD8Aa_po5GYZt5c_Bx8n-20DqCKpUyd51g",
     authDomain: "speedfest-8a08d.firebaseapp.com",
     databaseURL: "https://speedfest-8a08d.firebaseio.com",
@@ -8,26 +10,42 @@
     messagingSenderId: "701176021615",
     appId: "1:701176021615:web:8144bc21b550181181039a",
     measurementId: "G-D36RP2RRND"
-  }
-  
+  };
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
-  firebase.analytics()
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+  var db = firebase.firestore()
 
-  var firestore = firebase.firestore()
+  const usersRef = db.collection("first")
 
-    const docRef = firestore.doc("first/firstDoc")
-    const inputTextField = document.getElementById("name")
-    const saveButton = document.getElementById("submit")
+// Show current users on DB
+db.collection("users").get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data())
+      let obj = Object.values(doc.data())
+      let myJSON = JSON.stringify(obj)
+      var mySubString = myJSON.substring(
+        myJSON.lastIndexOf(',') + 2, 
+        myJSON.lastIndexOf('') -2
+      )
+      mySubString2 = myJSON.substring(2, myJSON.lastIndexOf(',') - 1)
+      create(mySubString)
+  })
+})
 
-    saveButton.addEventListener("click", function() {
-      const textToSave = inputTextField.value
-      console.log("I am going to save " + textToSave + " to Firestore")
-      docRef.set({
-        name: textToSave
-      }).then(function() {
-        console.log("Status saved!")
-      }).catch(function (error){
-        console.log("Got an error: ", error)
-      })
-    })
+// FireBase DB Triggers
+var docRef = db.collection("users").doc("user1");
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
--- js/background.js
+++ js/background.js
@@ -1,6 +1,20 @@
 var _guest = 0
 var _edit = 0
 var _names = [""]
+var _delete = ""
+
+function processForm(e) {
+    if (e.preventDefault) e.preventDefault();
+    // You must return false to prevent the default form behavior
+    return false;
+}
+
+var form = document.getElementById('register');
+if (form.attachEvent) {
+    form.attachEvent("submit", processForm);
+} else {
+    form.addEventListener("submit", processForm);
+}
 
 // Catch Submit button
 document.getElementById("register").addEventListener("submit", function () {
@@ -14,6 +28,15 @@ document.getElementById("register").addEventListener("submit", function () {
         document.getElementById("wrapper").style.opacity = "0.4"
         event.preventDefault()
     }
+    db.collection("users").doc(name).set({
+        username: name,
+        confirmed: "no"
+      }).then(function() {
+        console.log("User saved!")
+      }).catch(function (error){
+        console.log("Got an error: ", error)
+      })
+
 
     function names() {
         for (let i = 0; i < _names.length; i++) {
@@ -25,7 +48,8 @@ document.getElementById("register").addEventListener("submit", function () {
 })
 
 function create(name) {
-    event.preventDefault()
+    console.log("name on create: " + name)
+    //event.preventDefault()
     // Create the <li>
     let newLi = document.createElement("li")
     newLi.id = ("guest" + _guest)
@@ -34,9 +58,9 @@ function create(name) {
     newSpan = document.createElement("span")
     newSpan.id = ("edit" + _guest)
     newSpan = document.createElement("span")
-    newLi.appendChild(newSpan) //añade texto al div creado. 
-    let newContent = document.createTextNode(document.getElementById("name").value)
-    _names.push(document.getElementById("name").value)
+    newLi.appendChild(newSpan) //añade texto al div creado.
+    let newContent = document.createTextNode(name)
+    _names.push(name)
     console.log(_names)
     newSpan.appendChild(newContent)
 
@@ -50,11 +74,22 @@ function create(name) {
     newLabel.appendChild(newContent)
     newContent.type = 'checkbox'
     newLabel.appendChild(newContent)
+    if (mySubString2 == "yes"){
+        newContent.setAttribute("checked", "true")
+        newLabel.parentElement.className = "responded"
+    }
     newContent.addEventListener('change', function() {
         if(this.checked) {
+            console.log("my2: " + mySubString2)
             newLabel.parentElement.className = "responded"
+            db.collection("users").doc(name).update({
+                confirmed: "yes"
+            });
         } else {
             newLabel.parentElement.className = "not-responded"
+            db.collection("users").doc(name).update({
+                confirmed: "no"
+            });
         }
     })
     
@@ -67,14 +102,15 @@ function create(name) {
         modal.style.display = "block";
         //console.log(newButton.parentElement.firstElementChild)
         document.getElementById("newName").focus()
-
         //console.log(newButton.parentElement.firstElementChild)
         document.getElementById("newNameButton").onclick = function () {
             newButton.parentElement.firstElementChild.textContent = document.getElementById("newName").value
             modal.style.display = "none";
             document.getElementById("wrapper").style.opacity = "1"
+            db.collection("users").doc(name).update({
+                username: document.getElementById("newName").value
+            })
         }
-
         document.getElementById("newName").addEventListener("keyup", function (e) {
             if (e.keyCode === 13) {
                 newButton.parentElement.firstElementChild.textContent = document.getElementById("newName").value
@@ -95,10 +131,16 @@ function create(name) {
         document.getElementById("wrapper").style.opacity = "0.4"
         document.getElementById("delButton").onclick = function () {
             newButton2.parentElement.parentElement.removeChild(newLi)
+            console.log("name: " + name)
             remove.style.display = "none"
             document.getElementById("wrapper").style.opacity = "1"
             console.log(newLi.removeChild(newLi.lastChild))
             _names.splice(newLi.removeChild(newLi.lastChild))
+            db.collection("users").doc(name + "").delete().then(function() {
+                console.log("Document successfully deleted!");
+              }).catch(function(error) {
+                console.error("Error removing document: ", error);
+              })
         }
     })
     newLi.appendChild(newButton2)
@@ -153,11 +195,13 @@ span.onclick = function () {
 delClose.onclick = function () {
     remove.style.display = "none"
     document.getElementById("wrapper").style.opacity = "1"
+    document.getElementById("newName").value = ""
   }
 
 nameClose.onclick = function () {
     nameModal.style.display = "none"
     document.getElementById("wrapper").style.opacity = "1"
+    document.getElementById("newName").value = ""
   }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -165,6 +209,7 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none"
     document.getElementById("wrapper").style.opacity = "1"
+    document.getElementById("newName").value = ""
   }
 }
 
